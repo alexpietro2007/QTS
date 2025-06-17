38156d71740a31b5eb1f2de60257b4c3c6456e74
alexPietro <alexpietro.public@gmail.com>
2025-06-17 07:55:59 -0300

    arrumado

---
+++ loja/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const clienteRoutes = require('./src/router/router_cliente')
+const { testarConexao, conexao } = require('./src/DAO/conexao.js') // exemplo
+
+app.use(express.json())
+
+// Rotas baseadas na versão
+app.use('/firma/1.0.0', clienteRoutes)
+
+app.listen(process.env.PORTA, () => {
+  console.log(`Operando na porta ${process.env.PORTA}`)
+  testarConexao(conexao())
+})
